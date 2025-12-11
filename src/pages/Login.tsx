c821493ddce31d686c0437b1d586937c54676852
cyber08jk <jk.cyber2325@gmail.com>
2025-12-11 22:48:54 +0530

    new

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,7 +21,6 @@ export function Login() {
         <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-300 dark:bg-blue-900 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-xl opacity-20 animate-blob animation-delay-2000"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-pink-300 dark:bg-pink-900 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-xl opacity-20 animate-blob animation-delay-4000"></div>
       </div>
-
       <div className="max-w-md w-full space-y-8 relative z-10">
         {/* Logo and Header */}
         <div className="text-center">
