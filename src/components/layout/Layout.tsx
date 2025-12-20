ecc203dad4148466cc81b12a3e940a7a7cb8df1e
cyber08jk <jk.cyber2325@gmail.com>
2025-12-20 21:59:07 +0530

    error fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,6 +9,7 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-950">
       <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
