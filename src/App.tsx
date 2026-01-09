851073a2c9220c9b669788c6a793130a183acd79
cyber08jk <jk.cyber2325@gmail.com>
2026-01-09 22:11:30 +0530

    Add spacing before protected routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,7 @@ function App() {
                     <Route path="/test-connection" element={<TestConnection />} />
                     <Route path="/request-access" element={<RequestAccess />} />
 
+                    
                     {/* Protected routes with layout */}
                     <Route
                       path="/dashboard"
