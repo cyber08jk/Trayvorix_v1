4359a57f5a691eecd74d7e657a0b492b1ef4a8d8
cyber08jk <jk.cyber2325@gmail.com>
2026-01-21 21:43:07 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -95,6 +95,7 @@ function App() {
                         </ProtectedRoute>
                       }
                     />
+
                     <Route
                       path="/warehouses"
                       element={
